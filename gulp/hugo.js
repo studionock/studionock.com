21773d17f942cd8477416f1b0da5b674e8409811
Adam Bergman <adam@fransvilhelm.com>
2017-04-03 00:45:20 +0200

    Prettify Hugo output

--- gulp/hugo.js
+++ gulp/hugo.js
@@ -2,9 +2,15 @@ import cp from 'child_process';
 import { Writable } from 'stream';
 import gutil from 'gulp-util';
 
-function truncate(str, length = 100, end = '...') {
-  if (str.length < length) return str;
-  return `${str.substring(0, length - end.length)}${end}`;
+function truncate(str, length = 100) {
+  if (str.length < length) return `  ${str}`;
+
+  const regEx = new RegExp(`.{0,${length}}`, 'g');
+  const lines = str.match(regEx);
+  return lines.map((line) => {
+    const trimmed = line.trim();
+    return `  ${trimmed}`;
+  }).join('\n');
 }
 
 class Stream extends Writable {
@@ -12,6 +18,7 @@ class Stream extends Writable {
     const lines = chunk.toString()
       .split('\n')
       .filter(line => line !== '')
+      .map(line => line.trim())
       .map((line) => {
         const regEx = /(\d{2,}(\/|:)\d{2,}(\/|:)\d{2,})/;
         const words = line.split(' ');
@@ -21,18 +28,22 @@ class Stream extends Writable {
           .filter(str => !regEx.test(str))
           .join(' ');
 
-        let newHead = gutil.colors.grey(`  ${head}`);
-        if (head === 'INFO') newHead = gutil.colors.yellow(`  ${head}`);
-        if (head === 'WARN') newHead = gutil.colors.red(`  ${head}`);
-        if (head === 'Started' || head === 'Built') newHead = gutil.colors.white(`\n   ${head}`);
+        let newHead = gutil.colors.grey(`${head}`);
+        if (head === 'INFO') newHead = gutil.colors.yellow(`${head}`);
+        if (head === 'WARN') newHead = gutil.colors.red(`${head}`);
+        if (head === 'Started' || head === 'Built') newHead = gutil.colors.white(`${head}`);
 
         let newTail = gutil.colors.grey(tail);
         if (head === 'Started' || head === 'Built') newTail = gutil.colors.white(tail);
 
-        return truncate(`${newHead} ${newTail}`) + '\n'; // eslint-disable-line prefer-template
+        let newLine = '\n';
+        if (head === 'Started' || head === 'Built') newLine = '\n\n';
+
+        const joined = truncate(`${newHead} ${newTail}`);
+        return `${newLine}${joined}`; // eslint-disable-line prefer-template
       });
 
-    gutil.log('\n', ...lines);
+    gutil.log(...lines, '\n');
     cb();
   }
 }
