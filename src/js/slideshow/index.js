586d9a70855dd4802fbc9c8e9bd2eefb8cd281c6
Adam Bergman <adam@fransvilhelm.com>
2017-09-15 09:46:55 +0200

    :art: Update slideshow to use nock-slider

--- src/js/slideshow/index.js
+++ src/js/slideshow/index.js
@@ -1,29 +1,36 @@
-/* eslint-disable */
-import nockSlider from 'nock-slider';
+import nockSlider from 'nock-slider'; // eslint-disable-line
 import rafScheduler from 'raf-schd';
 import { getOuterHeight } from '../utils';
 
 const createListener = slideContainer =>
   rafScheduler(imgEl => {
     const height = getOuterHeight(imgEl);
-    slideContainer.style.height = `${height}px`;
+    slideContainer.style.height = `${height}px`; // eslint-disable-line
   });
 
-export default async function initSlider() {
+export default function initSlider() {
   try {
     const images = window.images ? JSON.parse(window.images) : null;
     const slideContainer = document.querySelector('.slideshow-container');
 
     if (images && slideContainer) {
-      const btnPrev = document.querySelector('.slideshowbtn-left');
+      const btnPrevious = document.querySelector('.slideshowbtn-left');
       const btnNext = document.querySelector('.slideshowbtn-right');
 
       const onSlideEnd = createListener(slideContainer);
 
-      await nockSlider(slideContainer, images, {
-        btnPrev,
+      const slider = nockSlider(slideContainer, images, {
+        transitionDuration: 500,
+        btnPrevious,
         btnNext,
         onSlideEnd,
+      });
+
+      window.addEventListener('keydown', event => {
+        const { keyCode } = event;
+        if (keyCode === 37 || keyCode === 39) event.preventDefault();
+        if (keyCode === 37) slider.previous();
+        if (keyCode === 39) slider.next();
       });
     }
   } catch (error) {
